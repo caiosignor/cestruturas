a11eb85863f61a469c7042c837f2703f4f80d6db
Carlos <caiosignor@gmail.com>
2020-04-26 15:17:08 -0300

    exemplo 1 funcionando

---
+++ lista/exemplo1/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "lib/clist/clist.h"
+
+
+
+void imprmirNodo(Musica *nodo){
+    printf("%s - %s\n",nodo->titulo,nodo->autor);
+}
+
+Musica criarMusica(char *titulo, char *autor, int duracao);
+
+int main(){
+    Lista *inicio;
+    Musica musica1;
+
+    musica1 = criarMusica("echoes", "pink floyd", 845);
+    inicio = adicionarElemento(inicio, musica1);
+
+    musica1 = criarMusica("sultoes do swing2", "dire straits", 2334);
+    adicionarElemento(inicio, musica1);
+
+    musica1 = criarMusica("sultoes do swing4", "dire straits", 2334);
+    adicionarElemento(inicio, musica1);
+
+    musica1 = criarMusica("sultoes do swing4", "dire straits", 2334);
+    adicionarElemento(inicio, musica1);
+    
+    for_each(inicio, imprmirNodo);
+    return 0;
+}
+
+Musica criarMusica(char *titulo, char *autor, int duracao){
+    Musica musica;
+    musica.titulo = titulo;
+    musica.autor = autor;
+    musica.duracao_em_segundos = duracao;
+    return musica;
+}
